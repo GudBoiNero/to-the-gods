a6ab665608c8ebd3df9a120379841381ea40cc43
Ben Champagne <donkeykong797@gmail.com>
2023-08-14 21:04:35 -0400

    these are some real chunky files

--- assets/player/Player.cs
+++ assets/player/Player.cs
@@ -3,6 +3,11 @@ using System;
 
 public partial class Player : LivingEntity
 {
+    [Export]
+    public AnimationPlayer animator;
+    [Export]
+    public Sprite2D swordSprite;
+
     [ExportGroup("Movement")]
     [Export]
     public float Speed = 120.0f;
@@ -14,6 +19,8 @@ public partial class Player : LivingEntity
     public float RollDuration = 0.5f;
     [Export]
     public float RollFriction = 0.2f;
+
+    public enum SpriteSheetRows { DOWN = 0, RIGHT_DOWN = 1, RIGHT = 2, RIGHT_UP = 3, UP = 4, LEFT_UP = 5, LEFT = 6, LEFT_DOWN = 7 }
 
     public enum PlayerState { FREE, ROLL, ATTACK, STANCE };
     private PlayerStateMachine stateMachine;
@@ -27,23 +34,29 @@ public partial class Player : LivingEntity
             return spriteSheetRow;
         }
     }
-
-    public Player() {
+    private int swordSpriteSheetRow = 0;
+    public int SwordSpriteSheetRow
+    {
+        get
+        {
+            swordSpriteSheetRow = GetSwordSpriteSheetRow();
+            return swordSpriteSheetRow;
+        }
+    }
+    public Player()
+    {
         stateMachine = new(this, PlayerState.FREE);
     }
-
     public override void _Ready()
     {
         base._Ready();
 
         AddChild(stateMachine);
     }
-
     public override void _UnhandledInput(InputEvent inputEvent)
     {
         // Handle rolling and attacking here
     }
-
     /// <summary>
     /// Refer to <c>resources/player/player-sheet.png</c> to understand the return values.
     /// </summary>
@@ -57,36 +70,94 @@ public partial class Player : LivingEntity
         {
             if (input.Y < 0)
             {
-                return 3; // Face up side
+                return (int)SpriteSheetRows.RIGHT_UP; // Face up side
             }
             else if (input.Y > 0)
             {
-                return 1; // Face down side
+                return (int)SpriteSheetRows.RIGHT_DOWN; // Face down side
             }
             else
             {
-                return 2;
+                return (int)SpriteSheetRows.RIGHT;
             }
         }
         else
         {
             if (input.Y < 0)
             {
-                return 4; // Face up
+                return (int)SpriteSheetRows.UP; // Face up
             }
             else if (input.Y > 0)
             {
-                return 0; // Face down
+                return (int)SpriteSheetRows.DOWN; // Face down
             }
         }
 
         return spriteSheetRow;
     }
+    /// <summary>
+    /// Refer to <c>resources/player/sword-sheet.png</c> to understand the return values.
+    /// </summary>
+    /// <returns></returns>
+    public int GetSwordSpriteSheetRow()
+    {
+        Vector2 input = Input.GetVector("left", "right", "up", "down");
 
+        // If left or right
+        if (input.X > 0)
+        {
+            if (input.Y < 0)
+            {
+                return (int)SpriteSheetRows.RIGHT_UP; // Face up right
+            }
+            else if (input.Y > 0)
+            {
+                return (int)SpriteSheetRows.RIGHT_DOWN; // Face down right
+            }
+            else
+            {
+                return (int)SpriteSheetRows.RIGHT;
+            }
+        }
+        else if (input.X < 0)
+        {
+            if (input.Y < 0)
+            {
+                return (int)SpriteSheetRows.LEFT_UP; // Face up left
+            }
+            else if (input.Y > 0)
+            {
+                return (int)SpriteSheetRows.LEFT_DOWN; // Face down left
+            }
+            else
+            {
+                return (int)SpriteSheetRows.LEFT;
+            }
+        }
+        else
+        {
+            if (input.Y < 0)
+            {
+                return (int)SpriteSheetRows.UP; // Face up
+            }
+            else if (input.Y > 0)
+            {
+                return (int)SpriteSheetRows.DOWN; // Face down
+            }
+        }
+
+        return swordSpriteSheetRow;
+    }
     public override void _LivingPhysicsProcess(double delta)
     {
         // Set sprite row
         sprite.FrameCoords = new(sprite.FrameCoords.X, SpriteSheetRow);
+        swordSprite.FrameCoords = new(swordSprite.FrameCoords.X, SwordSpriteSheetRow);
+        swordSprite.ZIndex = (
+            SwordSpriteSheetRow == (int)SpriteSheetRows.DOWN 
+            || SwordSpriteSheetRow == (int)SpriteSheetRows.LEFT_DOWN 
+            || SwordSpriteSheetRow == (int)SpriteSheetRows.RIGHT_DOWN
+        ) ? -1 : 0;
 
         Vector2 velocity = Velocity;
         Vector2 direction = Input.GetVector("left", "right", "up", "down").Normalized();
@@ -97,20 +168,36 @@ public partial class Player : LivingEntity
         Velocity = velocity;
         MoveAndSlide();
     }
-
+    /// <summary>
+    /// Determines left from right based off of player input.
+    /// </summary>
+    /// <returns></returns>
     public override int GetSpriteDirection()
     {
         float xInput = Input.GetVector("left", "right", "up", "down").X;
-        return xInput != 0 ? (int) Math.Round(xInput) : GetCurrentSpriteDirection;
+        return xInput != 0 ? (int)Math.Round(xInput) : GetCurrentSpriteDirection;
     }
-
-    private class PlayerStateMachine : StateMachine<PlayerState> {
+    private partial class PlayerStateMachine : StateMachine<PlayerState>
+    {
         private Player player;
 
-        public PlayerStateMachine(Player player, PlayerState startState) {
-            this.State = startState;
+        public PlayerStateMachine(Player player, PlayerState startState)
+        {
+            State = startState;
             this.player = player;
         }
-        
+
+        public override void _Enter(PlayerState oldState, PlayerState newState)
+        {
+
+        }
+        public override PlayerState _ChangeState(PlayerState oldState, PlayerState newState)
+        {
+            return newState;
+        }
+        public override void _Exit(PlayerState oldState, PlayerState newState)
+        {
+
+        }
     }
 }
--- assets/util/StateMachine.cs
+++ assets/util/StateMachine.cs
@@ -3,18 +3,27 @@ using System;
 
 public partial class StateMachine<TState> : Node where TState : Enum
 {
-    private TState state; 
-    public TState State {
-        get { return state; } 
-        set 
+    private TState state;
+    public TState State
+    {
+        get { return state; }
+        set
         {
-            state = _ChangeState(state, value);
+            TState oldState = state;
+            TState newState = value;
+
+            _Enter(oldState, newState);
+            state = _ChangeState(oldState, newState);
+            _Exit(oldState, newState);
         }
     }
 
-    public virtual TState _ChangeState(TState oldState, TState newState) {
-        
+    public virtual void _Enter(TState oldstate, TState newState) { }
 
+    public virtual TState _ChangeState(TState oldState, TState newState)
+    {
         return newState;
     }
+
+    public virtual void _Exit(TState oldstate, TState newState) { }
 }
